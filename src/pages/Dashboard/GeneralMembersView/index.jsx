3d644a2285c120d86210fd294696385fc5b1e4f6
Gautam Kumar <gautam.404@protonmail.com>
2023-05-20 01:15:40 +0530

    Sort General Member A to Z

--- src/pages/Dashboard/GeneralMembersView/index.jsx
+++ src/pages/Dashboard/GeneralMembersView/index.jsx
@@ -4,6 +4,16 @@ import GeneralMemberProfileCard from "./GeneralMemberProfileCard"
 
 
 function GeneralMembersView({ members, openContactDrawer }) {
+
+    // Sort Memebr By Name A to Z
+    members.sort((a, b) => {
+        return (function (a, b) {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+            return (a < b) ? -1 : (a > b) ? 1 : 0;
+        })(a.name, b.name);
+    });
+
     return <Grid container spacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 3, lg: 3 }}>
         {Array.from(members).map((memberProfileData, index) => (
             <Grid item xs={1} sm={1} md={1} lg={1} key={index}>
