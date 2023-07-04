bb85743a7fdfaca771123f89653cbfad07e2894e
Gautam Kumar <gautam.404@protonmail.com>
2023-07-05 02:20:55 +0530

    Search Algorithm In Members TAB

--- src/pages/Dashboard/GeneralMembersView/index.jsx
+++ src/pages/Dashboard/GeneralMembersView/index.jsx
@@ -1,9 +1,12 @@
-import { Grid } from "@mui/material"
+import { Box, Grid, IconButton, Input } from "@mui/material"
 import GeneralMemberProfileCard from "./GeneralMemberProfileCard"
+import { Close, Search } from "@mui/icons-material";
+import { useState } from "react";
 
 
 
 function GeneralMembersView({ members, openContactDrawer }) {
+    const [searchBarInputText, setSearchBarInputText] = useState('');
 
     // Sort Memebr By Name A to Z
     members.sort((a, b) => {
@@ -14,15 +17,41 @@ function GeneralMembersView({ members, openContactDrawer }) {
         })(a.fullName, b.fullName);
     });
 
-    return <Grid container spacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 3, lg: 3 }}>
-        {Array.from(members).map((memberProfileData, index) => (
-            <Grid item xs={1} sm={1} md={1} lg={1} key={index}>
-                <GeneralMemberProfileCard
-                    memberProfileData={memberProfileData}
-                    openContactDrawer={openContactDrawer} />
-            </Grid>
-        ))}
-    </Grid >
+    return <>
+        <Box
+            display="flex"
+            justifyContent="flex-end"
+            alignItems="flex-end"
+        >
+            <Input
+                sx={{ mr: { xs: 1, sm: 2, md: 5 }, mb: 1, "&.Mui-focused .MuiIconButton-root": { color: 'primary.main' } }}
+                placeholder='Search'
+                value={searchBarInputText}
+                onChange={(event) => { setSearchBarInputText(event.target.value) }}
+                endAdornment={
+                    <>
+                        <IconButton
+                            sx={{ visibility: searchBarInputText.length > 0 ? "visible" : "hidden" }}
+                            onClick={() => { setSearchBarInputText('') }}
+                        >
+                            <Close />
+                        </IconButton>
+                        <Search />
+                    </>
+                }
+            />
+        </Box>
+
+        <Grid container spacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 3, lg: 3 }}>
+            {Array.from(members).filter(x => x.fullName.toLowerCase().includes(searchBarInputText.toLowerCase())).map((memberProfileData, index) => (
+                <Grid item xs={1} sm={1} md={1} lg={1} key={index}>
+                    <GeneralMemberProfileCard
+                        memberProfileData={memberProfileData}
+                        openContactDrawer={openContactDrawer} />
+                </Grid>
+            ))}
+        </Grid >
+    </>
 }
 
 
